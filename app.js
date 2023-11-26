7cb4dd5c8db455c2b3c97a05381dadf3c1baba9a
ShivanG <shivangramola007@gmail.com>
2023-11-26 12:17:52 +0530

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// output div
 const result = document.querySelector(".result");
 // const textArea = document.querySelector("textarea");
-const bold = document.querySelector(".bold");
-const option = document.querySelector(".option");
+// const option = document.querySelector(".option");
 
 // buttons
 const rest = document.querySelector(".rest");
@@ -9,12 +9,16 @@ const words = document.querySelector(".words");
 const italic = document.querySelector(".italic");
 const underline = document.querySelector(".underline");
 const font = document.querySelector(".font");
-
-function isEmpty() {}
+const bold = document.querySelector(".bold");
 
 rest.addEventListener("click", () => {
+  const textArea = document.querySelector("textarea");
   textArea.value = "";
-  result.innerHTML = "";
+  textArea.style.fontWeight = "lighter";
+  textArea.style.fontStyle = "normal";
+  textArea.style.textDecorationLine = "none";
+  textArea.style.fontSize = "";
+  result.innerHTML = "<h2> everything is rest </h2>";
 });
 
 bold.addEventListener("click", () => {
@@ -33,17 +37,36 @@ words.addEventListener("click", () => {
   let data = textArea.value;
   const res = data.split(" ");
   result.innerHTML = `<h2> words : ${res.length} & characters : ${data.length} </h2>`;
+  if (data === "") result.innerHTML = `<h2> There is no text </h2>`;
 });
 
 italic.addEventListener("click", () => {
-  textArea.style.fontStyle = "italic";
+  const textArea = document.querySelector("textarea");
+  console.log(textArea.style.fontStyle);
+  if (textArea.style.fontStyle === "") {
+    textArea.style.fontStyle = "italic";
+  } else if (textArea.style.fontStyle === "italic") {
+    textArea.style.fontStyle = "normal";
+  } else if (textArea.style.fontStyle === "normal") {
+    textArea.style.fontStyle = "italic";
+  }
 });
 
 underline.addEventListener("click", () => {
-  textArea.style.textDecorationLine = "underline";
+  const textArea = document.querySelector("textarea");
+  console.log(textArea.style.textDecorationLine);
+  if (textArea.style.textDecorationLine === "") {
+    textArea.style.textDecorationLine = "underline";
+  } else if (textArea.style.textDecorationLine === "underline") {
+    textArea.style.textDecorationLine = "none";
+  } else if ((textArea.style.textDecorationLine = "none")) {
+    textArea.style.textDecorationLine = "underline";
+  }
 });
 
 font.addEventListener("click", () => {
+  const textArea = document.querySelector("textarea");
+  const option = document.querySelector(".option");
   let selectedIndex = option.selectedIndex;
   textArea.style.fontSize = option[selectedIndex].value;
 });
